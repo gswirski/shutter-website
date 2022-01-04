6e89a4fea633b72e439388ee035940b1e5ed6522
Grzegorz Świrski <grzegorz@swirski.name>
2022-01-04 17:13:57 +0100

    implement emails

--- pages/contact.js
+++ pages/contact.js
@@ -1,11 +1,42 @@
 /* This example requires Tailwind CSS v2.0+ */
 import Head from 'next/head'
 import Link from 'next/link'
+import { useState } from 'react'
 import Footer from '../components/footer'
 import Menu from '../components/menu'
 
-
 export default function Contact() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [camera, setCamera] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    //let isValidForm = handleValidation();
+
+      const res = await fetch("/api/sendgrid", {
+        body: JSON.stringify({
+            email: email,
+            name: name,
+            camera: camera,
+            message: message
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+      });
+
+      const { error } = await res.json();
+      if (error) {
+        console.log(error);
+        return;
+      }
+  };
+
+
   return (
     <div>
       <Head>
@@ -16,7 +47,7 @@ export default function Contact() {
       <Menu />
 
       <div className="py-12 bg-white">
-        <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="max-w-2xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="pb-12">
             <Link href="/contact">
               <a className="block lg:text-center default-a font-semibold tracking-wide uppercase">
@@ -26,13 +57,23 @@ export default function Contact() {
             <p className="lg:text-center mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
               Send Us A Message
             </p>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            Let’s talk 👋 Don’t hesitate to reach out with any question or issue you might have. We respond in less than 24 hours (except Sundays.)
+            </p>
+            <p className="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+            You can email us at <a href="mailto:help@shutter.dev" className="default-a">help@shutter.dev</a> or use the contact form below.
+            </p>
+
+
+
+
           </div>
 
-          <form action="#" method="POST">
+          <form onSubmit={handleSubmit}>
               <div className="shadow overflow-hidden sm:rounded-md">
                 <div className="px-4 py-5 bg-white sm:p-6">
                   <div className="grid grid-cols-6 gap-6">
-                    <div className="col-span-6 sm:col-span-4">
+                    <div className="col-span-6">
                       <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
                         Email address
                       </label>
@@ -41,6 +82,7 @@ export default function Contact() {
                         name="email-address"
                         id="email-address"
                         autoComplete="email"
+                        onChange={(e) => setEmail(e.target.value)}
                         className="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
@@ -54,6 +96,7 @@ export default function Contact() {
                         name="first-name"
                         id="first-name"
                         autoComplete="given-name"
+                        onChange={(e) => setName(e.target.value)}
                         className="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
@@ -67,6 +110,7 @@ export default function Contact() {
                         name="camera-model"
                         id="camera-model"
                         autoComplete="camera-model"
+                        onChange={(e) => setCamera(e.target.value)}
                         className="mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                       />
                     </div>
@@ -79,6 +123,7 @@ export default function Contact() {
                       <textarea
                         name="message-text"
                         id="message-text"
+                        onChange={(e) => setMessage(e.target.value)}
                         className="h-32 mt-1 focus:ring-red-500 focus:border-red-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                         ></textarea>
                     </div>
