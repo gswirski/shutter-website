9014915970733f9612f46145d6d74140c39d503e
Grzegorz Świrski <grzegorz@swirski.name>
2024-03-24 09:48:46 +0100

    add beta form redirect

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    async redirects() {
+      return [
+        {
+          source: '/beta',
+          destination: 'https://forms.gle/UXFj72t8Zd5BGmPL9',
+          permanent: false,
+          basePath: false
+        },
+      ]
+    },
+  };
+  
