85c475929ea79bb81ce868f5e1c1b0fc652ef790
Grzegorz Świrski <grzegorz@swirski.name>
2024-06-07 16:51:18 +0200

    create associated pages

---
+++ pages/fujifilm/applink/apple-watch-app.js
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import Footer from '../../../components/footer'
+import ShutterHead from '../../../components/head'
+import Header from '../../../components/header'
+
+export default function Home() {
+  return (
+    <div>
+      Yep, Apple Watch app is now available for Fujifilm!
+    </div>
+  )
+}
